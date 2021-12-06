999cd68eb29d1782e621176d9702c207c753fcda
gci_uoc <ggiovannyci@uoc.edu>
2021-12-06 07:13:27 +0100

    ok

---
+++ dist/p.1625f28e.js
@@ -0,0 +1,11 @@
+function menu() {
+    var x = document.getElementById("barraNav");
+    if (x.className === "nav") x.className += " responsive";
+    else x.className = "nav";
+}
+function animate() {
+    var x = document.getElementById("mysgv");
+    x.className = "active";
+}
+
+//# sourceMappingURL=p.1625f28e.js.map
